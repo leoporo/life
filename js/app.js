811160d6ec5bc6fc8e2eed031acbf886a54a92f6
Leonis Poroshtica <leonis.poroshtica@hotmail.com>
2022-03-16 22:54:20 +0100

    Modify navbar

--- js/app.js
+++ js/app.js
@@ -11,21 +11,26 @@ export function removeQuotes(value) {
 }
 
 function addPanelLink() {
+  const newLi = document.createElement("li");
   const panelLink = document.createElement("a");
   panelLink.href = "/panel.html";
   panelLink.innerHTML = "Paneli";
-  document.querySelector(".leftnav").appendChild(panelLink);
+  newLi.appendChild(panelLink);
+  document.querySelector(".left-nav").children[0].insertAdjacentElement("afterend", newLi);
 }
 
 if (sessionStorage.getItem("loggedIn") === "true") {
-  user = sessionStorage.getItem("currentUser");
-  const logoutBtnContent = '<a href="" id="logout">Ckyqu</a>';
-  document.querySelector("#auth").innerHTML =
-    "<span>Mire se vjen, " +
-    removeQuotes(JSON.stringify(JSON.parse(user).fullname)) +
-    ".</span>" +
-    logoutBtnContent;
   addPanelLink();
+  document.querySelector("#loginBtn").style.display = "none";
+  user = sessionStorage.getItem("currentUser");
+  const newLi = document.createElement("li");
+  const logoutBtnContent = document.createElement("a");
+  logoutBtnContent.href = "#";
+  logoutBtnContent.innerHTML = "Ckycu";
+  logoutBtnContent.id = "logout";
+  logoutBtnContent.style.alignSelf =  "flex-end";
+  newLi.appendChild(logoutBtnContent);
+  document.querySelector(".navbar-links").appendChild(newLi);
 }
 
 const logoutBtn = document.querySelector("#logout");
@@ -33,5 +38,10 @@ const logoutBtn = document.querySelector("#logout");
 if (logoutBtn !== null) {
   logoutBtn.addEventListener("click", function () {
     sessionStorage.clear();
+    window.location.href = "/";
   });
 }
+
+document.querySelector(".toggle-button").addEventListener("click", function(){
+  document.querySelector(".navbar-links").classList.toggle("active");
+});
