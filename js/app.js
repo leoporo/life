4b6ad27d955f8898cd4fd34bcd3902a0e24adad6
Leonis Poroshtica <leonis.poroshtica@hotmail.com>
2022-03-24 14:38:57 +0100

    Fix responsivity

--- js/app.js
+++ js/app.js
@@ -60,3 +60,28 @@ topBtn.addEventListener("click", function(){
     behavior: "smooth"
   });
 });
+
+document.querySelector("#menu").addEventListener("click", function(){
+  document.querySelector(".navbar-responsive").classList.add("active");
+});
+
+document.querySelector("#closemenu").addEventListener("click", function(){
+  document.querySelector(".navbar-responsive").classList.remove("active");
+});
+
+const menuItems = document.querySelectorAll(".navbar-responsive ul li a");
+
+ for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener("click", function() {
+    document.querySelector(".navbar-responsive").classList.remove("active");
+     });
+ }
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
