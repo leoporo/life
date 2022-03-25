bfb812018c505ba127e59ecc25ab19f0088cf260
Leonis Poroshtica <leonis.poroshtica@hotmail.com>
2022-03-25 14:28:09 +0100

    Copy header from index to other pages

--- js/appointment.js
+++ js/appointment.js
@@ -1,7 +1,12 @@
-document.querySelector("#scheduleForm").addEventListener("submit", function(e){
+document.querySelector("#appointmentForm").addEventListener("submit", function(e){
     e.preventDefault();
     this.reset();
+    document.querySelector("#appointmentForm select").style.color = "rgb(117, 117, 117)";
     const snackbar = document.querySelector("#snackbar")
     snackbar.classList.add("show");
     setTimeout(function(){ snackbar.classList.remove("show"); }, 2900);
+});
+
+document.querySelector("#appointmentForm select").addEventListener("change", function(){
+    document.querySelector("#appointmentForm select").style.color = "#006d77";
 });
