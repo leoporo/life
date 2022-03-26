2a49e19c14891cbd891ff66d7ec84b041ef65983
Leonis Poroshtica <leonis.poroshtica@hotmail.com>
2022-03-26 08:15:22 +0100

    Improve appointment form

--- js/appointment.js
+++ js/appointment.js
@@ -4,7 +4,7 @@ document.querySelector("#appointmentForm").addEventListener("submit", function(e
     document.querySelector("#appointmentForm select").style.color = "rgb(117, 117, 117)";
     const snackbar = document.querySelector("#snackbar")
     snackbar.classList.add("show");
-    setTimeout(function(){ snackbar.classList.remove("show"); }, 2900);
+    setTimeout(function(){ snackbar.classList.remove("show"); }, 3900);
 });
 
 document.querySelector("#appointmentForm select").addEventListener("change", function(){
